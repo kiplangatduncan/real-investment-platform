80a37e7ea3b806dd319487c5f5b545f54f50d741
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-13 22:23:53 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,1 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
-
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
