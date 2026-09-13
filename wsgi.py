10d7557f51424131688b82bbd771606909d85f1c
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-13 22:46:06 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+from app import app
+
+if __name__ == "__main__":
+    app.run()
+  
