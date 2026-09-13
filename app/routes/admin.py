9485dbf24b3b550158ed98cb076834ef73da4af1
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-13 18:27:49 +0300

    Update admin.py

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,1 +1,16 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from app.models import User
 
+admin = Blueprint("admin", __name__, url_prefix="/admin")
+
+
+@admin.route("/")
+@login_required
+def dashboard():
+    users = User.query.all()
+
+    return render_template(
+        "admin.html",
+        users=users
+    )
