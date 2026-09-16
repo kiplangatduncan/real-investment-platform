a1570ad7a11c086c31f48860c4e29c1d22f374a9
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-16 15:48:06 +0300

    Update __init__.py

--- app/mpesa/__init__.py
+++ app/mpesa/__init__.py
@@ -1,3 +1,11 @@
-from .mpesa import mpesa_bp
+from .mpesa import (
+    mpesa_bp,
+    initiate_stk_push,
+    query_stk_status,
+)
 
-__all__ = ["mpesa_bp"]
+__all__ = [
+    "mpesa_bp",
+    "initiate_stk_push",
+    "query_stk_status",
+]
