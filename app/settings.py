5a9e474579d11e81b5df2ce0a172f9f3ae9b2b4e
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-16 12:58:32 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,1 +1,19 @@
+import os
 
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///investment.db"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Payment settings
+    MPESA_CONSUMER_KEY = os.environ.get("MPESA_CONSUMER_KEY")
+    MPESA_CONSUMER_SECRET = os.environ.get("MPESA_CONSUMER_SECRET")
+    MPESA_SHORTCODE = os.environ.get("MPESA_SHORTCODE")
+    MPESA_PASSKEY = os.environ.get("MPESA_PASSKEY")
+    MPESA_CALLBACK_URL = os.environ.get("MPESA_CALLBACK_URL")
